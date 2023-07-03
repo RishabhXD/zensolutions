83d5e93a748f2ebc00d989ae142b14779e8db88f
RishabhXD <rishabh@futurestic.co.in>
2023-07-04 04:10:59 +0530

    bold

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -10,7 +10,7 @@ const Tabs = () => {
 
   return (
     <div className="max-w-screen-lg mx-auto">
-      <div className="flex border-b border-gray-200">
+      <div className="flex flex-wrap border-b border-gray-200">
         <TabButton isActive={activeTab === 1} onClick={() => handleTabClick(1)}>
           Our Mission
         </TabButton>
@@ -21,7 +21,7 @@ const Tabs = () => {
           Our Values
         </TabButton>
       </div>
-      <div className="mt-4 px-12">
+      <div className="mt-4 px-4 sm:px-8 md:px-12">
         {activeTab === 1 && (
           <TabContent>
             <h2 className="text-lg font-semibold mb-2">Our Mission</h2>
@@ -50,55 +50,66 @@ const Tabs = () => {
         {activeTab === 3 && (
           <TabContent>
             <h2 className="text-lg font-semibold mb-2">Our Values</h2>
-            <ul className="list-none pl-6">
+            <ul className="list-none pl-4 sm:pl-6">
               <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
-                  <span>Customer Focus – We put our customers first.</span>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
-                  <span>Caring – We are concerned about you.</span>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
                   <span>
-                    Simplicity – We try to keep it as straightforward as
-                    possible.
+                    <strong>Customer Focus</strong> – We put our customers
+                    first.
                   </span>
                 </div>
               </li>
               <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
-                  <span>Passion – We are passionate about serving you.</span>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
                   <span>
-                    Commitment – We are dedicated to achieving excellence.
+                    <strong>Caring</strong> – We are concerned about you.
                   </span>
                 </div>
               </li>
               <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
                   <span>
-                    Integrity – We conduct ourselves in a trustworthy manner.
+                    <strong>Simplicity</strong> – We try to keep it as
+                    straightforward as possible.
                   </span>
                 </div>
               </li>
               <li>
-                <div className="flex items-center">
-                  <TiTick className="text-[#9333ea] mr-2" />
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
                   <span>
-                    Professionalism – We take our work very seriously.
+                    <strong>Passion</strong> – We are passionate about serving
+                    you.
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
+                  <span>
+                    <strong>Commitment</strong> – We are dedicated to achieving
+                    excellence.
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
+                  <span>
+                    <strong>Integrity</strong> – We conduct ourselves in a
+                    trustworthy manner.
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-start">
+                  <TiTick className="text-[#9333ea] mt-1 mr-2" />
+                  <span>
+                    <strong>Professionalism</strong> – We take our work very
+                    seriously.
                   </span>
                 </div>
               </li>
